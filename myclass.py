1fcab74723af4a5c4b7080e549eae79efc120dba
Nathan Agarwal <nathan.agarwal@yahoo.com>
2019-05-26 23:02:42 -0600

    Add class to code again

---
+++ myclass.py
@@ -0,0 +1,19 @@
+class MyClass(object):
+
+    def __init__(self, age = 45, name = "Jose"):
+        self._age = age
+        self._name = name
+
+    def getName(self):
+        return self._name
+
+    def setAge(self, age):
+        self._age = age
+
+    def getAge(self):
+        return self._age
+
+    def setName(self, name):
+        self._name = name
+
+        
